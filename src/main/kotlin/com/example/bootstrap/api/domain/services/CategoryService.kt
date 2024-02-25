05a228c1ca12410e4e2e115a72164cdcb65a40d5
ronaldoomjr <ronaldoomjunior@gmail.com>
2024-02-25 14:48:50 -0300

    inserting data with POST

--- src/main/kotlin/com/example/bootstrap/api/domain/services/CategoryService.kt
+++ src/main/kotlin/com/example/bootstrap/api/domain/services/CategoryService.kt
@@ -1,8 +1,13 @@
 package com.example.bootstrap.api.domain.services
 
+import com.example.bootstrap.api.domain.dtos.CategoryDTO
+import com.example.bootstrap.api.domain.dtos.toEntity
+import com.example.bootstrap.api.infra.persistence.entities.toDTO
 import com.example.bootstrap.api.infra.persistence.repositories.CategoryRepository
 import org.springframework.stereotype.Service
 
 @Service
 class CategoryService(private val categoryRepository: CategoryRepository) {
+    fun save(categoryDTO: CategoryDTO): CategoryDTO
+            = categoryRepository.save(categoryDTO.toEntity()).toDTO()
 }
--- src/main/kotlin/com/example/bootstrap/api/web/controllers/CategoryController.kt
+++ src/main/kotlin/com/example/bootstrap/api/web/controllers/CategoryController.kt
@@ -1,10 +1,22 @@
 package com.example.bootstrap.api.web.controllers
 
+import com.example.bootstrap.api.domain.dtos.CategoryDTO
 import com.example.bootstrap.api.domain.services.CategoryService
+import jakarta.validation.Valid
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
+import org.springframework.web.util.UriComponentsBuilder
 
 @RestController
 @RequestMapping("/categories")
 class CategoryController(private val categoryService: CategoryService) {
+    @PostMapping
+    fun save(@RequestBody @Valid categoryDTO: CategoryDTO, uriBuilder: UriComponentsBuilder):
+            ResponseEntity<CategoryDTO> {
+        val newCategoryDTO = categoryService.save(categoryDTO)
 
+        val uri = uriBuilder.path("/categories/{id}").buildAndExpand(newCategoryDTO.id).toUri()
+
+        return ResponseEntity.created(uri).body(newCategoryDTO)
+    }
 }
